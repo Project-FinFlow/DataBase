4568a6944f2e593b68fac9950b42cce9039eb50a
gsccsz <guilhermesoprano1227@gmail.com>
2026-03-18 22:32:16 -0300

    Atualizado banco de dados, incluso 'triggers.sql'.

---
+++ DataBase/triggers.sql
@@ -0,0 +1,76 @@
+DELIMITER //  /*Triggers receita.*/
+
+CREATE TRIGGER after_insert_receita
+AFTER INSERT ON receita
+FOR EACH ROW
+BEGIN
+
+UPDATE saldo
+SET 
+    saldo_atual = saldo_atual + NEW.valor,
+    total_receitas = total_receitas + NEW.valor
+WHERE usuario_id = NEW.usuario_id;
+
+END //
+
+DELIMITER ;
+
+/*Triggers despesa.*/
+ 
+DELIMITER //
+
+CREATE TRIGGER after_insert_despesa
+AFTER INSERT ON despesas
+FOR EACH ROW
+BEGIN
+
+UPDATE saldo
+SET 
+    saldo_atual = saldo_atual - NEW.valor,
+    total_despesas = total_despesas + NEW.valor
+WHERE usuario_id = NEW.usuario_id;
+
+END //
+
+DELIMITER ;
+
+v
+
+DELIMITER //
+
+CREATE TRIGGER log_receita_auto
+AFTER INSERT ON receita
+FOR EACH ROW
+BEGIN
+
+INSERT INTO logs(usuario_id,acao,tabela_afetada,descricao)
+VALUES(NEW.usuario_id,'INSERT','receita','Receita adicionada automaticamente');
+
+END //
+
+DELIMITER ;
+
+/*Triggers para impedir saldo negativo*/
+
+DELIMITER //
+
+CREATE TRIGGER before_insert_despesa
+BEFORE INSERT ON despesas
+FOR EACH ROW
+BEGIN
+
+DECLARE saldo_atual_usuario DECIMAL(10,2);
+
+SELECT saldo_atual
+INTO saldo_atual_usuario
+FROM saldo
+WHERE usuario_id = NEW.usuario_id;
+
+IF saldo_atual_usuario < NEW.valor THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Saldo insuficiente';
+END IF;
+
+END //
+
+DELIMITER ;
